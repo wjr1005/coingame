ff73203d9a05ce9c7039b67ed298937918ea886f
coingame <webmaster@coingame.io>
2015-07-04 20:41:23 +0800

    Signed-off-by: coingame <webmaster@coingame.io>

--- web.js
+++ web.js
@@ -1,13 +1,12 @@
 var express = require('express')
+	, compress = require('compression')
     , path = require('path')
     , app = express()
     , server = require('http').createServer(app);
 
 app.set('port', process.env.PORT || 80);
-//app.use(express.favicon());
-//app.use(express.logger('dev'));
-//app.use(express.bodyParser());
-//app.use(express.methodOverride());
+app.use(require('method-override')());
+app.use(compress());
 app.use(express.static(path.join(__dirname, './client-web')));
 
 if (app.get('env') === 'development') {
